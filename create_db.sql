5136f436b7511171a4b7fa6cb616992508658862
Rubabdrani <rtbdrani@gmail.com>
2025-04-30 09:42:43 +0300

    first update

---
+++ create_db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE text_to_pic;
---
+++ main_app/views.py
@@ -0,0 +1,41 @@
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+# Login
+class LoginView(APIView):
+
+  def post(self, request):
+    try:
+      username = request.data.get('username')
+      password = request.data.get('password')
+      user = authenticate(username=username, password=password)
+      if user:
+        refresh = RefreshToken.for_user(user)
+        content = {'refresh': str(refresh), 'access': str(refresh.access_token),'user': UserSerializer(user).data}
+        return Response(content, status=status.HTTP_200_OK)
+      return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    except Exception as err:
+      return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# User Registration
+class CreateUserView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+  def create(self, request, *args, **kwargs):
+    try:
+      response = super().create(request, *args, **kwargs)
+      user = User.objects.get(username=response.data['username'])
+      refresh = RefreshToken.for_user(user)
+      content = {'refresh': str(refresh), 'access': str(refresh.access_token), 'user': response.data }
+      return Response(content, status=status.HTTP_201_CREATED)
+    except Exception as err:
+      return Response({ 'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+

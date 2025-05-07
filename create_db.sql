c25abc28b254c1fc710e6073a574f1b16ed5748e
rubabdran <rtbdrani@gmail.com>
2025-05-07 12:22:29 +0300

    organizing

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,12 @@
-CREATE DATABASE text_to_pic;
+-- PostgreSQL --
+
+--Create a new database
+CREATE DATABASE text_to_pic; 
+
+--Create username and password 
 CREATE USER admin_1 WITH PASSWORD 'password';
+
+--Gives the user admin_1 full permissions
 GRANT ALL PRIVILEGES ON DATABASE text_to_pic TO admin_1;
 GRANT ALL ON SCHEMA public TO admin_1;
 GRANT ALL ON SCHEMA public TO public;
